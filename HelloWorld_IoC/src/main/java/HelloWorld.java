719e95b3637cf57adafb23b761e6a61d7c7b3c13
Dmitriy Shelemekh <d.shelemekh@gmail.com>
2018-04-05 15:47:48 +0300

    IoC HelloWold

--- HelloWorld_IoC/src/main/java/HelloWorld.java
+++ HelloWorld_IoC/src/main/java/HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world !");
+//        System.out.println("Hello world !");
+
+        MessageRenderer mr = new StamdartOutMessageRenderer();
+        MessageProvider mp = new HelloWorldMessageProvider();
+        mr.setMessageProvider(mp);
+        mr.render();
     }
 }
---
+++ HelloWorld_IoC/src/main/java/StamdartOutMessageRenderer.java
@@ -0,0 +1,25 @@
+public class StamdartOutMessageRenderer implements MessageRenderer {
+    private MessageProvider messageProvider;
+
+    @Override
+    public void render() {
+        if (messageProvider == null) {
+            throw new RuntimeException(
+                    "You must set the property messageProvider of class StamdartOutMessageRenderer"
+                            + StamdartOutMessageRenderer.class.getName());
+
+        }
+
+        System.out.println(messageProvider.getMessage());
+    }
+
+    @Override
+    public void setMessageProvider(MessageProvider messageProvider) {
+        this.messageProvider = messageProvider;
+    }
+
+    @Override
+    public MessageProvider getMessageProvider() {
+        return this.messageProvider;
+    }
+}
